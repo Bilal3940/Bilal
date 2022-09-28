5e9f81562268ef30d72a2afad531d8df6a0a5ff8
Bilal Javaid <109686186+Bilal3940@users.noreply.github.com>
2022-09-28 22:05:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
     <Routes>
     <Route path='/' element={<Layout/>}>
     <Route index element={<Home />} exact/>
-    
     <Route path = "/about" element={<About />} />
     <Route path = "/contact" element={<Contact />} />
     </Route>
